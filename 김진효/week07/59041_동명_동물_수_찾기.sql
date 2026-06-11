85c4628a0306779e472fff3ad2478b60b51f841a
ppnyoong <ppnyoong@gmail.com>
2026-06-11 15:03:15 +0900

    chore: sql 문제 혼동 수정

--- 김진효/week07/59041_동명_동물_수_찾기.sql
+++ 김진효/week07/59041_동명_동물_수_찾기.sql
@@ -1,7 +1,8 @@
 # 문제: 동명 동물 수 찾기
 # URL: https://school.programmers.co.kr/learn/courses/30/lessons/59041
 
-SELECT animal_id, name 
+SELECT name, COUNT(name) as COUNT 
 FROM animal_ins 
-WHERE animal_type = 'Dog' AND LOWER(name) LIKE '%el%' 
-ORDER BY name, animal_id;
+GROUP BY name 
+HAVING COUNT(name) >= 2 
+ORDER BY name;
--- 김진효/week07/59047_이름에_el이_들어가는_동물_찾기.sql
+++ 김진효/week07/59047_이름에_el이_들어가는_동물_찾기.sql
@@ -1,8 +1,7 @@
 # 문제: 이름에 el이 들어가는 동물 찾기
 # URL: https://school.programmers.co.kr/learn/courses/30/lessons/59047
 
-SELECT name, COUNT(name) as COUNT 
+SELECT animal_id, name 
 FROM animal_ins 
-GROUP BY name 
-HAVING COUNT(name) >= 2 
-ORDER BY name;
+WHERE animal_type = 'Dog' AND LOWER(name) LIKE '%el%' 
+ORDER BY name, animal_id;
