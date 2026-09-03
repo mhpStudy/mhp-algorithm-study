5da3a9ecf5fef816356210a5c9e3ec054b737608
So8oro <voo870@gmail.com>
2026-09-03 14:32:38 +0900

    solve week 17 +

--- 김현우/week17/157341_대여_기록이_존재하는_자동차_리스트_구하기.sql
+++ 김현우/week17/157341_대여_기록이_존재하는_자동차_리스트_구하기.sql
@@ -1,5 +1,9 @@
-# 문제: 대여 기록이 존재하는 자동차 리스트 구하기
-# URL: https://school.programmers.co.kr/learn/courses/30/lessons/157341
+-- 문제: 대여 기록이 존재하는 자동차 리스트 구하기
+-- URL: https://school.programmers.co.kr/learn/courses/30/lessons/157341
 
-def solution():
-    pass
+SELECT DISTINCT C.CAR_ID
+FROM CAR_RENTAL_COMPANY_CAR C
+JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H ON C.CAR_ID = H.CAR_ID
+WHERE C.CAR_TYPE = '세단'
+  AND MONTH(H.START_DATE) = 10
+ORDER BY C.CAR_ID DESC;
--- 김현우/week17/59043_있었는데요_없었습니다.sql
+++ 김현우/week17/59043_있었는데요_없었습니다.sql
@@ -1,5 +1,10 @@
-# 문제: 있었는데요 없었습니다
-# URL: https://school.programmers.co.kr/learn/courses/30/lessons/59043
+-- 문제: 있었는데요 없었습니다
+-- URL: https://school.programmers.co.kr/learn/courses/30/lessons/59043
 
-def solution():
-    pass
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_INS A
+JOIN ANIMAL_OUTS O
+    ON A.ANIMAL_ID = O.ANIMAL_ID
+WHERE A.DATETIME > O.DATETIME
+ORDER BY A.DATETIME
